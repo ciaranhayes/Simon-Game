c5f2db1e88fedcefaa7b295c63d6b4cf76a49ad4
Ciaran Hayes <hayes.c.hayes@gmail.com>
2025-03-12 23:44:49 +0100

    sounds working and starting game works

--- game.js
+++ game.js
@@ -1,8 +1,32 @@
 
 let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
+let userClickedPattern = [];
+let level = 0
+let started = false;
+
+$(document).keydown(function () {
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence()
+        started = true;
+    }
+});
+
+$(".btn").click(function () {
+    let userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+})
 
 function nextSequence() {
+
+    level++;
+    $(".btn").on("click", function() {
+        $("#level-title").text("Level " + level);
+    })
+    $("level-title").text("Level " + level);
 
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColour = buttonColours[randomNumber];
@@ -10,17 +34,30 @@ function nextSequence() {
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
+    playSound(randomChosenColour);
+}
 
-    let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+function playSound(name) {
+    let audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
 
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass( "pressed");
+    setTimeout(function () {
+        $("#" + currentColour).removeClass( "pressed" );
+    }, 100);
+}
 
-$(document).click(function (event) {
-    console.log("clicked");
-    let userChosenColour = $(".btn ").on("click", function () {
-        let id = this.id;
-        console.log(id);
-    })
-})
+
+
+
+
+
+
+
+
+
+
+
 
